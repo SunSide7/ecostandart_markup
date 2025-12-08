ae5df8453ab89d6613c5ed16a42575325af199fb
Ascended <email@email.ru>
2025-12-08 13:25:52 +0200

    [13:25 08.12.2025] Legend 65

--- js/assets/script.js
+++ js/assets/script.js
@@ -32,6 +32,40 @@ $(document).ready(function() {
         }
     })
 
+    $('.img-slider-x4').owlCarousel({
+        // loop:true,
+        margin:16,
+        nav:true,
+        dots: false,
+        navText: [
+            '<svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M4.75 0.749999L0.75 5.25L4.75 9.75" stroke="#3A434D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '</svg>\n',
+            '<svg width="6" height="11" viewBox="0 0 6 11" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M0.750001 0.749999L4.75 5.25L0.75 9.75" stroke="#3A434D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+            '</svg>',
+        ],
+        responsive:{
+            360: {
+                items: 1
+            },
+            640: {
+                items: 2
+            },
+            960: {
+                items: 4
+            },
+            1280: {
+                items: 4
+            },
+            1920: {
+                items: 4
+            },
+        }
+    })
+
+
+
 
     function setCardsSliderActive() {
         $('.cards-slider').addClass('owl-theme');
@@ -111,8 +145,6 @@ $(document).ready(function() {
 
         setVizierPos()
     }
-
-
 
     setVizierPos()
 
