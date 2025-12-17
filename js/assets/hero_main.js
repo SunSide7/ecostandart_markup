46e9a86fda19d421bf74c23ab9f48e887f8ee989
Ascended <email@email.ru>
2025-12-18 00:09:14 +0200

    [0:09 18.12.2025] hero_asc

---
+++ js/assets/hero_main.js
@@ -0,0 +1,71 @@
+function setVizierPos() {
+
+    $('.hero-asc-scaffold .hero-asc-text').height()
+
+    var scaffold = $('.hero-asc-scaffold')[0]
+
+    var title = $('.hero-asc-scaffold-content')[0];
+    var style = title.currentStyle || window.getComputedStyle(title);
+
+    console.log('bottom:', style.bottom.replace('px', ''))
+
+
+
+    $('.hero-asc-line-h').css('top', $('.hero-asc-scaffold .hero-asc-text-top')[0].clientHeight + scaffold.clientHeight - (title.clientHeight + +style.bottom.replace('px', '')) + 'px');
+    $('.hero-asc-line-v').css('left', $('.hero-asc-scaffold .hero-asc-text')[0].clientWidth + +style.marginLeft.replace('px', '') + +style.paddingLeft.replace('px', '') + 44 + 'px');
+    $('.hero-asc-line-cross').css('top', $('.hero-asc-scaffold .hero-asc-text-top')[0].clientHeight - ($('.hero-asc-line-cross')[0].clientHeight / 2) + scaffold.clientHeight - (title.clientHeight + +style.bottom.replace('px', '')) + 3 + 'px');
+    $('.hero-asc-line-cross').css('left', $('.hero-asc-scaffold .hero-asc-text')[0].clientWidth + +style.marginLeft.replace('px', '') + +style.paddingLeft.replace('px', '') - ($('.hero-asc-line-cross')[0].clientWidth / 2) + 3 + 44 + 'px');
+
+}
+
+function setStartContent() {
+    $('.hero-asc-scaffold .hero-asc-title-top').text($('.hero-asc-slide.active .hero-asc-title-top').text())
+    $('.hero-asc-scaffold .hero-asc-title').text($('.hero-asc-slide.active .hero-asc-title').text())
+    $('.hero-asc-scaffold .hero-asc-subtitle').text($('.hero-asc-slide.active .hero-asc-subtitle').text())
+
+    $('.hero-asc-scaffold').css({'background-image': 'url(' + $('.hero-asc-slide.active img').attr('src') + ')',});
+
+    setVizierPos()
+}
+
+function setNextContent() {
+    $('.hero-asc-slide.active').next().addClass('active').prev().removeClass('active')
+    $('.hero-asc-scaffold .hero-asc-title-top').text($('.hero-asc-slide.active .hero-asc-title-top').text())
+    $('.hero-asc-scaffold .hero-asc-title').text($('.hero-asc-slide.active .hero-asc-title').text())
+    $('.hero-asc-scaffold .hero-asc-subtitle').text($('.hero-asc-slide.active .hero-asc-subtitle').text())
+
+    $('.hero-asc-scaffold').css({'background-image': 'url(' + $('.hero-asc-slide.active img').attr('src') + ')'});
+
+    setVizierPos()
+}
+
+function setPrevContent() {
+    $('.hero-asc-slide.active').prev().addClass('active').next().removeClass('active')
+    $('.hero-asc-scaffold .hero-asc-title-top').text($('.hero-asc-slide.active .hero-asc-title-top').text())
+    $('.hero-asc-scaffold .hero-asc-title').text($('.hero-asc-slide.active .hero-asc-title').text())
+    $('.hero-asc-scaffold .hero-asc-subtitle').text($('.hero-asc-slide.active .hero-asc-subtitle').text())
+
+    $('.hero-asc-scaffold').css({'background-image': 'url(' + $('.hero-asc-slide.active img').attr('src') + ')'});
+
+    setVizierPos()
+}
+
+setVizierPos()
+setStartContent();
+
+$(window).on('resize', () => {
+    setVizierPos()
+    setStartContent();
+});
+
+$(window).on('wheel', () => {
+    if(event.ctrlKey == true) {
+        setVizierPos()
+        setStartContent();
+    }
+});
+
+
+
+$('body').on('click', '.hero-asc-next', setNextContent)
+$('body').on('click', '.hero-asc-prev', setPrevContent)
