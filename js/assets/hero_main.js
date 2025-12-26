6494e4c7c490ae682aa12ec9db74d5a3bfb2d1ef
Ascended <email@email.ru>
2025-12-26 11:16:03 +0200

    [11:16 26.12.2025] Changes

--- js/assets/hero_main.js
+++ js/assets/hero_main.js
@@ -30,13 +30,25 @@ function setStartContent() {
     $('.hero-asc-btn-wrp').empty()
     $('.hero-asc-slide.active button').clone().appendTo('.hero-asc-btn-wrp')
 
+    if ($('.hero-asc-slide.active').hasClass('space-bottom-30')) {
+        $('.hero-asc-scaffold').addClass('space-bottom-30')
+    } else {
+        $('.hero-asc-scaffold').removeClass('space-bottom-30')
+    }
+
 
     setVizierPos()
 
 }
 
 function setNextContent() {
-    $('.hero-asc-slide.active').next().addClass('active').prev().removeClass('active')
+    if($('.hero-asc-slide.active').hasClass('last')) {
+        $('.hero-asc-slide.last').removeClass('active')
+        $('.hero-asc-slide.first').addClass('active')
+    } else {
+        $('.hero-asc-slide.active').next().addClass('active').prev().removeClass('active')
+    }
+
     $('.hero-asc-scaffold .hero-asc-title-top').text($('.hero-asc-slide.active .hero-asc-title-top').text())
     $('.hero-asc-scaffold .hero-asc-title').text($('.hero-asc-slide.active .hero-asc-title').text())
     $('.hero-asc-scaffold .hero-asc-subtitle').text($('.hero-asc-slide.active .hero-asc-subtitle').text())
@@ -45,12 +57,23 @@ function setNextContent() {
 
     $('.hero-asc-btn-wrp').empty()
     $('.hero-asc-slide.active button').clone().appendTo('.hero-asc-btn-wrp')
+
+    if ($('.hero-asc-slide.active').hasClass('space-bottom-30')) {
+        $('.hero-asc-scaffold').addClass('space-bottom-30')
+    } else {
+        $('.hero-asc-scaffold').removeClass('space-bottom-30')
+    }
 
     setVizierPos()
 }
 
 function setPrevContent() {
-    $('.hero-asc-slide.active').prev().addClass('active').next().removeClass('active')
+    if($('.hero-asc-slide.active').hasClass('first')) {
+        $('.hero-asc-slide.first').removeClass('active')
+        $('.hero-asc-slide.last').addClass('active')
+    } else {
+        $('.hero-asc-slide.active').prev().addClass('active').next().removeClass('active')
+    }
     $('.hero-asc-scaffold .hero-asc-title-top').text($('.hero-asc-slide.active .hero-asc-title-top').text())
     $('.hero-asc-scaffold .hero-asc-title').text($('.hero-asc-slide.active .hero-asc-title').text())
     $('.hero-asc-scaffold .hero-asc-subtitle').text($('.hero-asc-slide.active .hero-asc-subtitle').text())
@@ -59,6 +82,12 @@ function setPrevContent() {
 
     $('.hero-asc-btn-wrp').empty()
     $('.hero-asc-slide.active button').clone().appendTo('.hero-asc-btn-wrp')
+
+    if ($('.hero-asc-slide.active').hasClass('space-bottom-30')) {
+        $('.hero-asc-scaffold').addClass('space-bottom-30')
+    } else {
+        $('.hero-asc-scaffold').removeClass('space-bottom-30')
+    }
 
     setVizierPos()
 }
